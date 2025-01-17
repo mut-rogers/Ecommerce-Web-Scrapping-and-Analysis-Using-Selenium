f7591a30caef4706dd4c581d6ea258bbde1962bc
Muthoni Rogers <rogersmacharia77@gmail.com>
2025-01-17 06:16:15 +0300

    updated settings to include product categories

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 
-
 """ 
 This module holds the project settings including 
 - Constants 
@@ -9,4 +8,12 @@ This module holds the project settings including
 
 # URL for the website.
 # product category and web page number will change on demand
-BASE_URL = "https://www.kilimall.co.ke/category/{}?id=968&form=category&page={}"
+BASE_URL = "https://www.kilimall.co.ke/category/{}?id={}&form=category&page={}"
+
+# Contains product categories available on the page 
+# and those which will be scrapped. 
+# The product category has a unique id and category name as stored on the dict
+PRODUCT_CATEGORIES = {
+    "167": "clothes",
+    "1": "bags"
+}
