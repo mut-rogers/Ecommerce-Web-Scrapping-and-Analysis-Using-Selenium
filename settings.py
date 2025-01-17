80ffcfd7cceabb8edc3acfac656d2d2326addb7d
Muthoni Rogers <rogersmacharia77@gmail.com>
2025-01-17 06:55:06 +0300

    added args to scrappe_page to include product category

--- settings.py
+++ settings.py
@@ -6,14 +6,9 @@ This module holds the project settings including
 """
 
 
-# URL for the website.
-# product category and web page number will change on demand
-BASE_URL = "https://www.kilimall.co.ke/category/{}?id={}&form=category&page={}"
+
 
 # Contains product categories available on the page 
 # and those which will be scrapped. 
 # The product category has a unique id and category name as stored on the dict
-PRODUCT_CATEGORIES = {
-    "167": "clothes",
-    "1": "bags"
-}
+
