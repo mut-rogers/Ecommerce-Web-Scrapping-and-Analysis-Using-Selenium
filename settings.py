899d4a07f1067aee0ff339e5aec3959a9389d552
Muthoni Rogers <rogersmacharia77@gmail.com>
2025-01-17 05:42:04 +0300

    updated main and scrape_page methods. OK

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+
+""" 
+This module holds the project settings including 
+- Constants 
+- Common variables etc
+"""
+
+
+# URL for the website.
+# product category and web page number will change on demand
+BASE_URL = "https://www.kilimall.co.ke/category/{}?id=968&form=category&page={}"
